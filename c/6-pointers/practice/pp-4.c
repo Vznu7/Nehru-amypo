a04959387508d1c40db2784ec64a717a49d3ed8c
vznu7 <vishnuvishnu49373@gmail.com>
2025-07-20 22:05:46 +0530

    added pointers p4

--- c/6-pointers/practice/pp-4.c
+++ c/6-pointers/practice/pp-4.c
@@ -1,1 +1,19 @@
 // it contains the code without using pointers
+#include <stdio.h>
+int main()
+{
+    int a;
+    if(scanf("%d",&a)!=1||a<=0||a>1000)
+    {
+        printf("Invalid Input");
+        return 0;
+    }
+    float arr[1000];
+    for(int i =0;i<a;i++)
+    {
+        scanf("%f ",&arr[i]);
+        printf("%.2f ",arr[i]*2);
+        
+    }
+    
+}
