1746e95900471f6f89cb4f33388a224600b3fcdf
vznu7 <vishnuvishnu49373@gmail.com>
2025-07-21 11:24:46 +0530

    added unsolved problems in loop

--- c/3-loops/practice/lp-4.c
+++ c/3-loops/practice/lp-4.c
@@ -1,1 +1,25 @@
-// i cant find the logic if anyone found the answer send the push request
+#include <stdio.h>
+int main()
+{
+    int n ,odd =1;
+    scanf("%d",&n);
+    if(n<0)
+    {
+        printf("Invalid input");
+        return 0;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int s=0;s<n-i;s++)
+        {
+            printf(" ");
+        }
+        for(int j =0;j<i;j++)
+        {
+            printf("%d ",odd);
+            odd+=2;
+        }
+        printf("\n");
+    }
+    return 0;
+}
--- c/3-loops/workout/lw-5.c
+++ c/3-loops/workout/lw-5.c
@@ -1,1 +1,32 @@
-// i cant find the logic if anyone found the answer send the push request
+#include<stdio.h>
+
+void main()
+{
+    int n;
+    if(scanf("%d",&n)!=1)
+    {
+        printf("Invalid Input\n");
+    }
+    if(n<0 || n>=10)
+    {
+        printf("Invalid Input\n");
+    }
+    int size =2*n-1;
+    for(int i=0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+            int top=i;
+            int left=j;
+            int right=size-1-j;
+            int bottom= size-1-i;
+            int min=top;
+            if(left<min)min=left;
+            if(right<min)min=right;
+            if(bottom<min)min=bottom;
+            printf("%d",n-min);
+            
+        }
+        printf("\n");
+    }
+}
